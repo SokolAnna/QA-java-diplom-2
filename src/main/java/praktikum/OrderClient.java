d822da895c8d37c8af07bb950ab54739adadd9a5
Anna Sokolova <iambirdann@yandex.ru>
2022-05-03 13:11:20 +0300

    Updated OrderClient

--- src/main/java/praktikum/OrderClient.java
+++ src/main/java/praktikum/OrderClient.java
@@ -21,6 +21,17 @@ public class OrderClient extends MainClient {
                 .then();
     }
 
+    @Step("Create order {token} one ingredient")
+    public ValidatableResponse createOrderOneIngredient(String accessToken, ArrayList<String> ingredients) {
+        return given()
+                .spec(getBaseSpec())
+                .header("Authorization", accessToken)
+                .body("{\"ingredients\": [\"" + ingredients.get(0) + "\"]}")
+                .when()
+                .post(ORDER_PATH)
+                .then();
+    }
+
     @Step("Create empty order {token}")
     public ValidatableResponse createEmptyOrder(String accessToken) {
         return given()
